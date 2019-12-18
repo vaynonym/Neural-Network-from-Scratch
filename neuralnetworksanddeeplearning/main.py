09d6682866cae8f88ae7fdae9a103e3b2163d7f3
Vaynonym <timruschke@googlemail.com>
2019-12-18 23:26:46 +0100

    dunno

--- neuralnetworksanddeeplearning/main.py
+++ neuralnetworksanddeeplearning/main.py
@@ -2,7 +2,6 @@ from network import Network
 import dataReader
 
 training_data, validation_data, test_data = dataReader.load_data_wrapper()
-
-net = Network([784,30,10])
+net = Network([784, 30, 30, 30, 30, 30, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, testData = test_data)
 
