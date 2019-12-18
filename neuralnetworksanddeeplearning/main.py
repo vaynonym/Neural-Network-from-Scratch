40c22557fc22cb0ea8dbd067ca95b76f491d89c9
unknown <tr91kozy@studserv.uni-leipzig.de>
2019-12-18 07:58:43 +0100

    minimal change

--- neuralnetworksanddeeplearning/main.py
+++ neuralnetworksanddeeplearning/main.py
@@ -1,5 +1,6 @@
 from network import Network
+import dataReader as dataReader
 
-net = Network([10,5,3])
-
-print(net)
+trainingData, validationData, testData = dataReader.load_data_wrapper()
+net = Network([28*28,50, 10])
+net.SGD(trainingData, 28, 10, 3.0, testData)
