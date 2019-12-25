b833a941c87f25c7409a5632c4dc1034a9c42ec2
Vaynonym <timruschke@googlemail.com>
2019-12-25 01:16:22 +0100

    implemented dropout with smaller dropout for input layer and no dropout for the last two layers (to enable correction). Also scales output accordingly when not in training mode and dropout is generally activated.

--- neuralnetworksanddeeplearning/pytorch comparison/feedforward.py
+++ neuralnetworksanddeeplearning/pytorch comparison/feedforward.py
@@ -13,27 +13,48 @@ class InvalidNumberOfLayersError(Exception):
         self.message = message
 
 class FeedforwardNet(nn.Module):
-    def __init__(self, sizes_of_layers, activation_functions):
+    def __init__(self, sizes_of_layers, activation_functions, USE_DROPOUT = False, dropout_probability = 0):
         super(FeedforwardNet, self).__init__()
         if(len(sizes_of_layers) != len(activation_functions) + 1):
             raise LengthMismatchError("Length of sizes_of_layers must be length of activation_function_of_layers - 1")
         self.number_layers = len(activation_functions)
         if(self.number_layers < 1):
             raise InvalidNumberOfLayersError("A NN must have at least one layer")
+        self.USE_DROPOUT = USE_DROPOUT
         self.activations = activation_functions
         modulelist = []
+        dropout_modulelist = []
         for i in range(self.number_layers):
+        
             modulelist.append(nn.Linear(sizes_of_layers[i], sizes_of_layers[i+1]))
+            
+            
         self.module_list  = nn.ModuleList(modulelist)
+        
+        self.dropout = nn.Dropout(dropout_probability)
+        self.dropout_input = nn.Dropout(0.2)
+        self.dropout_probability = dropout_probability
+
     
     def forward(self, x):
         i = 0
+        if(self.USE_DROPOUT):
+            x = self.dropout_input(x)
         
         for module in self.module_list:
+
             x = module(x)
             # apparently the last layer of activations breaks the cost function, so we ignore them
-            if(i != (len(self.module_list) - 1)):
+            
+            if(i != (len(self.module_list) - 1) ):
                 x = self.activations[i](x)
+            
+            if(self.USE_DROPOUT):
+                if(i != (len(self.module_list) - 2) ):
+                    x = self.dropout(x)
+                    if(not self.training): # to compensate for training with not all nodes active and the ensuing higher weights
+                        x = x * (1-self.dropout_probability)
+                
             
             i = i + 1
         return x
