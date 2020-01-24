30602e3ada4007928db85643ba3334c24411777c
Vaynonym <timruschke@googlemail.com>
2020-01-24 17:03:17 +0100

    fixed

--- neuralnetworksanddeeplearning/pytorch comparison/sampling.py
+++ neuralnetworksanddeeplearning/pytorch comparison/sampling.py
@@ -2,6 +2,11 @@ import numpy as np
 from skimage import io, transform
 from PIL import Image
 import matplotlib.pyplot as plt # for plotting
+import torchvision.transforms as transforms # for modifying vision data to run it through models
+
+sample_translation = transforms.RandomAffine(0, translate=(0.15, 0.15))
+sample_rotate = transforms.RandomRotation(10)
+sample_scale = transforms.RandomAffine(0, scale=(0.7, 1.3))
 
 def load_image(filename):
     img = Image.open(filename)
@@ -15,11 +20,11 @@ def show_image(image, label):
     plt.show()
     
 
-def label_number(labelvector):
-    labelvector = [labelvector[i] * i for i in range(len(labelvector))]
+def label_number(label):
+    labelvector = [1 if(i == label) else 0 for i in range(label)]
     return max(labelvector)
 
-def examples():
+def examples(training_data):
     print(np.array(training_data).shape)
 
     sample = list(training_data)[0]
@@ -29,4 +34,14 @@ def examples():
     # rearrange data from a simple vector to a matrix representing the image
     # then changing values into standard 8 bit values and converting to image:
     im = Image.fromarray(sampleArray.reshape(28,28) *255)
-    show_image(im, label_number(sampleLabel))
+    show_sample_transformations(im, sampleLabel)
+
+def show_sample_transformations(image, label):
+    show_image(image, label)
+    image_translated = sample_translation(image)
+    show_image(image_translated, label)
+    image_rotated = sample_rotate(image)
+    show_image(image_rotated, label)
+    image_scaled = sample_scale(image)
+    show_image(image_scaled, label)
+    
