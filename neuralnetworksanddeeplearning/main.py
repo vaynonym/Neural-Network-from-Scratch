bc5922248b989b061e2aa605ecf20f16bf4ebf0f
unknown <tr91kozy@studserv.uni-leipzig.de>
2020-01-29 18:48:58 +0100

    prepared parameters for presentation

--- neuralnetworksanddeeplearning/main.py
+++ neuralnetworksanddeeplearning/main.py
@@ -3,4 +3,4 @@ import dataReader
 
 training_data, validation_data, test_data = dataReader.load_data_wrapper()
 net = Network([28*28,50, 10])
-net.SGD(training_data, 28, 10, 3.0, test_data)
+net.SGD(training_data, 20, 10, 3.0, test_data)
