2d69e291d6a7b36218661b774fd6c071b45ac00c
Vaynonym <timruschke@googlemail.com>
2020-01-29 18:52:54 +0100

    Preparing for presentation

--- neuralnetworksanddeeplearning/pytorch comparison/hiragana.py
+++ neuralnetworksanddeeplearning/pytorch comparison/hiragana.py
@@ -36,6 +36,16 @@ def test_network():
         correct += (predicted == labels).sum()
     return (correct, total)
 
+transformation = transforms.Compose([
+    transforms.ToTensor(),
+    # add noise which adds or subtracts proportionally to x. This way, 
+    # every value that started as 0 will remain 0 and we won't have negative values 
+    transforms.Lambda(lambda x : x + 0.03 * x * (2 * (torch.rand(x.shape)) - 1)),
+    transforms.ToPILImage(),
+    transforms.RandomAffine(degrees = 5.0, translate = (0.04, 0.04), scale=(0.9, 1.1)),
+    transforms.ToTensor()
+    ])
+
 CUDA_FLAG = False # determines whether the GPU is used for calculations
 TRAINING_ERROR = True # determines whether or not the accuracy on the trainingset will be calculated and logged
 LOAD_STATE = False # determines whether or not the initial state of the NN's weights and biases will be loaded from a file
@@ -47,16 +57,16 @@ torch.manual_seed(RANDOM_SEED)
 # np.random.seed(RANDOM_SEED)
 
 # hyperparameters
-NUMBER_OF_EPOCHS = 15
-BATCH_SIZE = 50
-LEARNING_RATE = 2 * 1e-1
+NUMBER_OF_EPOCHS = 10
+BATCH_SIZE = 300
+LEARNING_RATE = 1 * 1e-2
 MOMENTUM = 0.5
 USE_DROPOUT = False
 dropout_probability = 0.5
 
 # network topology and activation functions
-sizes_of_layers = [784, 150, 50, 30, 10]
-activation_functions = [ F.relu, F.relu, F.relu, F.relu]
+sizes_of_layers = [784, 50, 50, 49]
+activation_functions = [ F.relu, F.relu, F.relu]
 activation_functions_string = "[F.relu, F.relu, F.relu, F.relu]"
 
 # Load and prepare data for use
@@ -66,8 +76,21 @@ training_data, validation_data, test_data = dataReader.load_hiragana()
 
 
 
-training_dataset = MNIST_Dataset(training_data)
+training_dataset = MNIST_Dataset(training_data, transform=None)
 
+
+'''
+print(training_data[0][0])
+print(training_dataset.__getitem__(0)[0].shape)
+print(len(training_data))
+print(validation_data[0][0].shape)
+print(len(validation_data))
+print(test_data[0][0].shape)
+print(len(test_data))
+print(type(training_data[0][0]))
+print(type(training_data[0][1]))
+print(training_data[0][0].dtype)
+'''
 
 
 trainloader = torch.utils.data.DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
@@ -79,9 +102,45 @@ net = FeedforwardNet(sizes_of_layers, activation_functions, USE_DROPOUT=USE_DROP
 if(CUDA_FLAG):
     net = net.cuda()
 
-for data in training_data[8:20]:
-    sampling.examples(data)
+# print(net(training_data[0][0]))
 
+# for data in training_data[8:20]:
+#    sampling.examples(data)
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+
+# Determine where the state of the neural network will be saved or loaded
+SAVE_STATE_PATH = "NN_States_Hiragana/" + str(sizes_of_layers) + "_best_state.pt"
+LOAD_STATE_PATH = "NN_States_Hiragana/Best/" + str(sizes_of_layers) + "_best_state.pt"
+if(LOAD_STATE):
+    # loads all layers
+    # net.load_NN_state(LOAD_STATE_PATH)
+    # loads all layers excluding the last one and works even if model has more layers afterward
+    net.load_NN_state(LOAD_STATE_PATH)
+
+# initialize the logger to log the training and results
+log = laTeX_log( 
+    RANDOM_SEED, NUMBER_OF_EPOCHS, BATCH_SIZE, LEARNING_RATE, MOMENTUM, 
+    sizes_of_layers, activation_functions_string, 
+    len(training_data), len(validation_data), len(test_data),
+    loss_function, str(optimizer).split(" ")[0] + "()", LOG_TRAINING_SET=TRAINING_ERROR
+)
+
+# the big function
+train_network(net, optimizer, NUMBER_OF_EPOCHS, loss_function, trainloader, validationloader, log, SAVE_STATE_PATH, CUDA_FLAG, TRAINING_ERROR)
+
+# testing state of the network at the end of training
+correct, total = test_network()
+print('Accuracy on the test set after training: {} out of {}'.format(correct, total))
+
+# testing state of the network at the time of best performance on the validation set
+net.load_NN_state(SAVE_STATE_PATH)
+correct, total = test_network()
+print('Best Iteration: Accuracy on the test set: {} out of {}'.format(correct, total))
+log.add_testset_result(int(correct))
+
+log.write_to_file("outputfile_laTeX.txt")
 
 
 
